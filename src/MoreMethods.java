98fd0218a30f2631492bc628089f7cabf417a4f3
Stephanie Curd <stephaniecurd@gmail.com>
2022-03-09 20:27:59 -0500

    All Methods

--- src/MoreMethods.java
+++ src/MoreMethods.java
@@ -8,11 +8,18 @@ public class MoreMethods {
 		myArray[2] = 8;
 
 
-		System.out.println(sumArray(myArray));
+		System.out.println(sumArray(myArray));	
 		
+		double[] grades = new double[5];
+		grades[0] = 88.7;
+		grades[1] = 92.5;
+		grades[2] = 100;
+		grades[3] = 67.3;
+		grades[4] = 78.9;
 		
-	}
-	
+		System.out.println(calculateAverage(grades));
+		System.out.println(multiplyString("Hello", 3));
+	}	
 	
 //Takes an array of ints and returns the sum of all ints
 	
@@ -22,9 +29,33 @@ public class MoreMethods {
 			for (int number : numbers) {
 				sum += number;
 			}
-			return sum;
+			return sum;	
+	}
+
+//	Takes an array of double and returns the average of all elements in the array	
+		
+	public static double calculateAverage(double[] numbers) {
+		double sum = 0;
+		for (double number : numbers) {
+			sum += number;
+		}
+		return sum / numbers.length;
+			
+			
+	}
+		
+//	Take a string and an int an returns the string concatenated with itself int number of times
+	
+	
+	public static String multiplyString(String str, int num) {
+		String result = "";
+		for (int i = 0; i < num; i++){
+			result += str;
+		}
+		return result;
 		
 		
 	}
+		}
+	
 
-}
